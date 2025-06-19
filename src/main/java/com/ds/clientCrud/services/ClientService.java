91da5a187d1c7b8fd95c01175aadad602da16ded
matheus augusto <math99@outlook.com>
2025-06-19 18:17:49 -0300

    Busca por id retorna 404 para cliente inexistente

--- src/main/java/com/ds/clientCrud/services/ClientService.java
+++ src/main/java/com/ds/clientCrud/services/ClientService.java
@@ -3,10 +3,10 @@ package com.ds.clientCrud.services;
 import com.ds.clientCrud.dto.ClientDTO;
 import com.ds.clientCrud.entities.Client;
 import com.ds.clientCrud.repositories.ClientRepository;
+import com.ds.clientCrud.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +20,9 @@ public class ClientService {
 
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
-        Client client = repository.findById(id).get();
+        Client client = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado")
+        );
         return new ClientDTO(client);
     }
 
---
+++ src/main/java/com/ds/clientCrud/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ds.clientCrud.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException (String msg){
+        super(msg);
+    }
+}
